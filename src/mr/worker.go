55ed153f2303b52e00268b005dcc03024ca96556
史祥宇 <sxy@dataqin.com>
2021-08-26 13:49:00 +0800

    use tcp

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -138,9 +138,9 @@ func doReport(requestMsg *RequestMsg) {
 // returns false if something goes wrong.
 //
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := masterSock()
-	c, err := rpc.DialHTTP("unix", sockname)
+	c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	//sockname := masterSock()
+	//c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
